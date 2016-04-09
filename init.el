ebc62e2b7a79e57d85640625981a15bd5dd87263
William Stubler <kafkabro@fatherTron-Manjaro.Home>
2016-04-09 16:23:27 -0700

    added machine specific stuff

--- init.el
+++ init.el
@@ -71,3 +71,24 @@
 
 ;; - buffer-move -
 (use-package buffer-move :ensure t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+(defun wgs85/fathertron-config ()
+  (custom-set-faces
+   ;; custom-set-faces was added by Custom.
+   ;; If you edit it by hand, you could mess it up, so be careful.
+   ;; Your init file should contain only one such instance.
+   ;; If there is more than one, they won't work right.
+   '(default ((t (:inherit nil :stipple nil :background "#181a26" :foreground "gray80"
+			   :inverse-video nil :box nil :strike-through nil :overline nil
+			   :underline nil :slant normal :weight normal :height 90
+			   :width normal :foundry "PfEd" :family "DejaVu Sans Mono")))))
+  )
+
+(when (string= system-name "fatherTron-Manjaro.Home")
+  (wgs85/fathertron-config))
