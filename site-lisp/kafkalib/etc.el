a2d7431dac7df93fa95de6d9429792c7d32410a0
kafkabro <william.stubler@gmail.com>
2016-09-17 23:40:32 -0700

    added modal time stamps!

--- site-lisp/kafkalib/etc.el
+++ site-lisp/kafkalib/etc.el
@@ -1,7 +1,10 @@
-(defun kafkalib/org-insert-current-time-stamp (is_active)
+(defun kafkalib/org-insert-current-time-stamp (ts_mode)
   "inserts org formatted time-stamp with current time, add prefix arg for active"
-  (interactive "P")
-  (org-insert-time-stamp (current-time) "t" (not is_active)))
+  (interactive "p")
+  (if (= ts_mode 1)  (org-insert-time-stamp (current-time) "t" 1)
+    (if (= ts_mode 4) (insert (format-time-string "%Y-%m-%d"))
+      (insert (substring (format-time-string (org-time-stamp-format 1 1)) 1 -1)))))
+
 
 (defun kafkalib/launch-shell (arg)
   "launches a shell with a unique name"
