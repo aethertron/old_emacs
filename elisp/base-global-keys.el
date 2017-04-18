d0c413b296f3e94deb4ca01640fab13551dfb468
nooSphere <william.stubler@gmail.com>
2017-04-18 19:48:51 -0400

    added ctrl-tab behavior

--- elisp/base-global-keys.el
+++ elisp/base-global-keys.el
@@ -1,6 +1,7 @@
 ;; Add your keys here, as such
 
 ;(global-set-key (kbd "[SHORTCUT]") '[FUNCTION])
-
+(global-set-key (kbd "<C-tab>") 'next-multiframe-window)
+(global-set-key (kbd "<C-S-iso-lefttab>") 'previous-multiframe-window)
 
 (provide 'base-global-keys)
