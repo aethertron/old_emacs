d1c066498ccb8338eb583cccc43080ca73b31501
nooSphere <william.stubler@gmail.com>
2017-04-18 19:39:31 -0400

    finally a simple init!

---
+++ init.el
@@ -0,0 +1,28 @@
+;;; package --- Main init file
+;;; Commentary:
+;;; This is my init file
+
+;;; Code:
+
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'load-path (concat user-emacs-directory "elisp"))
+
+(require 'base)
+(require 'base-theme)
+(require 'base-extensions)
+(require 'base-functions)
+(require 'base-global-keys)
+
+(require 'lang-python)
+
+(require 'lang-javascript)
+
+(require 'lang-web)
+
+(require 'lang-c)
