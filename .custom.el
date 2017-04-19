39b064dc77d7227a02148ab59084ba42a3236077
nooSphere <william.stubler@gmail.com>
2017-04-18 23:18:43 -0600

    Bringing in find-file-at-point!

--- .custom.el
+++ .custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" default))))
+ '(counsel-find-file-at-point t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
