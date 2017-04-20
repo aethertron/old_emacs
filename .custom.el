a9acfc3785a8d7f7b3e73199e5a8155246fc7009
nooSphere <william.stubler@gmail.com>
2017-04-20 16:58:12 -0700

    stuff

--- .custom.el
+++ .custom.el
@@ -3,7 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(counsel-find-file-at-point t))
+ '(js2-strict-inconsistent-return-warning nil)
+ '(js2-strict-missing-semi-warning nil)
+ '(package-selected-packages
+   (quote
+    (company-c-headers ggtags impatient-mode web-mode js2-refactor js-comint company-tern tern js2-mode pyenv-mode py-autopep8 pip-requirements elpy yasnippet wgrep which-key undo-tree smex smartparens org-bullets org-projectile neotree multiple-cursors magit hlinum counsel-projectile counsel flycheck expand-region exec-path-from-shell elpakit dashboard company avy spacemacs-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
