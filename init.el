56e71585bb20d5b9e883a1ac5295c9a3d9bb936b
nooSphere <william.stubler@gmail.com>
2017-04-20 16:59:04 -0700

    added bootstrap

--- init.el
+++ init.el
@@ -13,16 +13,30 @@
 
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 
+(require 'elpa-bootstrap)
 (require 'base)
+
+;; local packages (may rely on other pkgs, install after elpa)
+(add-to-list 'load-path "~/.emacs.d/elisp-pkgs/elpa-mirror")
+(require 'elpa-mirror)
+
+;; ;; test code here
+;; (package-install 'ivy)
+;; (require 'ivy)
+;; (ivy-mode)
+
 (require 'base-theme)
 (require 'base-extensions)
 (require 'base-functions)
 (require 'base-global-keys)
 
 (require 'lang-python)
-
 (require 'lang-javascript)
-
 (require 'lang-web)
-
 (require 'lang-c)
+
+;; back-up and create local copy if not using local already
+(if (null use-local)
+    (progn
+      (setq elpamr-default-output-directory "~/.emacs.d/myelpa")
+      (elpamr-create-mirror-for-installed)))
