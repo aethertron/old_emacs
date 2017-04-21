25bc303e216fcf96b22e8e0681774b5e0ff0b595
nooSphere <william.stubler@gmail.com>
2017-04-21 16:01:56 -0700

    vim-style open line behavior

--- elisp/06-base-global-keys.el
+++ elisp/06-base-global-keys.el
@@ -1,9 +1,13 @@
 ;; Add your keys here, as such
+;; Assume previous files have been sourced, not need to require or refer to them!
 
 ;(global-set-key (kbd "[SHORTCUT]") '[FUNCTION])
 (global-set-key (kbd "<C-tab>") 'next-multiframe-window)
 (global-set-key (kbd "<C-S-iso-lefttab>") 'previous-multiframe-window)
 (global-set-key (kbd "C-h A") 'customize-group)
 (global-set-key (kbd "C-c h") 'hl-line-mode)
+
+(define-key global-map [remap open-line] 'wgs-open-next-line)
+(define-key global-map (kbd "C-S-o") 'wgs-open-prev-line)
 
 (provide '06-base-global-keys)
