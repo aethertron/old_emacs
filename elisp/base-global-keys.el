dc57f7ecb8af2f99b5512761db65f4449021a699
nooSphere <william.stubler@gmail.com>
2017-04-20 19:12:28 -0700

    renamed

--- elisp/base-global-keys.el
+++
@@ -1,9 +0,0 @@
-;; Add your keys here, as such
-
-;(global-set-key (kbd "[SHORTCUT]") '[FUNCTION])
-(global-set-key (kbd "<C-tab>") 'next-multiframe-window)
-(global-set-key (kbd "<C-S-iso-lefttab>") 'previous-multiframe-window)
-(global-set-key (kbd "C-h A") 'customize-group)
-(global-set-key (kbd "C-c h") 'hl-line-mode)
-
-(provide 'base-global-keys)
--- elisp/base-theme.el
+++
@@ -1,8 +0,0 @@
-(use-package spacemacs-theme
-  :defer t
-  :init
-  (load-theme 'spacemacs-dark t))
-
-(set-face-attribute 'default nil :height 120)
-
-(provide 'base-theme)
