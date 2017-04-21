f41a819e77366a962e55de94dde4f56208a1d714
nooSphere <william.stubler@gmail.com>
2017-04-20 18:32:45 -0700

    hl mode

--- elisp/base-global-keys.el
+++ elisp/base-global-keys.el
@@ -4,5 +4,6 @@
 (global-set-key (kbd "<C-tab>") 'next-multiframe-window)
 (global-set-key (kbd "<C-S-iso-lefttab>") 'previous-multiframe-window)
 (global-set-key (kbd "C-h A") 'customize-group)
+(global-set-key (kbd "C-c h") 'hl-line-mode)
 
 (provide 'base-global-keys)
