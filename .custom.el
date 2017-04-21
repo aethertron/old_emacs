18fcc98248e2f9af18e4f87ad94df941cdaac7cc
nooSphere <william.stubler@gmail.com>
2017-04-20 20:02:17 -0700

    more package config

--- .custom.el
+++ .custom.el
@@ -5,7 +5,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (buffer-move which-key wgrep web-mode use-package undo-tree spacemacs-theme smex smartparens pyenv-mode py-autopep8 pip-requirements org-projectile org-bullets neotree magit js2-refactor js-comint impatient-mode hlinum ggtags flycheck expand-region exec-path-from-shell elpy elpakit dashboard counsel-projectile company-tern company-c-headers avy))))
+    (dired+ diredx dired-x which-key wgrep web-mode use-package undo-tree spacemacs-theme smex smartparens pyenv-mode py-autopep8 pip-requirements org-projectile org-bullets neotree magit js2-refactor js-comint impatient-mode hlinum ggtags flycheck expand-region exec-path-from-shell elpy elpakit dashboard counsel-projectile company-tern company-c-headers buffer-move avy))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
