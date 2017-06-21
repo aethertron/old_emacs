8f9bc3ee3d2787410e104f51a9decfcee4f0e012
bgs <william.stubler@gmail.com>
2017-06-21 12:17:50 -0700

    cleanup up packages

--- elisp/01-elpa-bootstrap.el
+++ elisp/01-elpa-bootstrap.el
@@ -10,18 +10,14 @@
     (progn
       (setq package-archives ())
       (add-to-list 'package-archives
-		   ;; '("myelpa" . "http://localhost:8000/")) ;; works!
 		   '("myelpa" . "~/.emacs.d/myelpa/"))
-      ;; ("myelpa" . "https://raw.githubusercontent.com/redguardtoo/myelpa/master")
       package-archives)
    ;; use web
   (progn
     (setq package-archives ())
     (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
     (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-    (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
     (add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
-    ;; (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
     package-archives))
 
 (when (not package-archive-contents)
