18816207596e97600765fcdf9030e02131e3885f
bgs <william.stubler@gmail.com>
2017-08-16 17:45:28 -0700

    using eog for png for now

--- elisp/02-base.el
+++ elisp/02-base.el
@@ -80,6 +80,8 @@
 (delete (list "\\.pdf\\'" "xpdf") dired-guess-shell-alist-default)
 (add-to-list 'dired-guess-shell-alist-default (list "\\.pdf\\'" "okular"))
 (add-to-list 'dired-guess-shell-alist-default (list "\\.html\\'" "google-chrome"))
+(delete (list "\\.png\\'" "display") dired-guess-shell-alist-default)
+(add-to-list 'dired-guess-shell-alist-default (list "\\.png\\'" "eog"))
 
 ;; Write comint input rings (for history, etc) when buffers are killed
 (use-package dash)
