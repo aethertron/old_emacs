1401fae798bc4c32df10a78c18df1c3436fa0c0f
bgs <william.stubler@gmail.com>
2017-10-11 11:13:57 -0700

    remove tern for now

--- elisp/lang-javascript.el
+++ elisp/lang-javascript.el
@@ -20,19 +20,21 @@
   ;; tern :- IDE like features for javascript and completion
   ;; http://ternjs.net/doc/manual.html#emacs
   (use-package tern
-    :config
-    (defun my-js-mode-hook ()
-      "Hook for `js-mode'."
-      (set (make-local-variable 'company-backends)
-           '((company-tern company-files))))
-    (add-hook 'js2-mode-hook 'my-js-mode-hook)
-    (add-hook 'js2-mode-hook 'company-mode))
+    ;; :config
+    ;; (defun my-js-mode-hook ()
+    ;;   "Hook for `js-mode'."
+    ;;   (set (make-local-variable 'company-backends)
+    ;;        '((company-tern company-files))))
+    ;; (add-hook 'js2-mode-hook 'my-js-mode-hook)
+    )
 
-  (add-hook 'js2-mode-hook 'tern-mode)
+  (add-hook 'js2-mode-hook 'company-mode)
 
-  ;; company backend for tern
-  ;; http://ternjs.net/doc/manual.html#emacs
-  (use-package company-tern)
+  ;; (add-hook 'js2-mode-hook 'tern-mode)
+
+  ;; ;; company backend for tern
+  ;; ;; http://ternjs.net/doc/manual.html#emacs
+  ;; (use-package company-tern)
 
   ;; Run a JavaScript interpreter in an inferior process window
   ;; https://github.com/redguardtoo/js-comint
