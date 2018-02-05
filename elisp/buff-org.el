2e77e943c3d414b25d577bf24e4e716df5f74022
bgs-blue <william.stubler@gmail.com>
2018-02-05 12:00:34 -0700

    cleaned up buf-org, using idiomatic use-package expressions, sorted setting of variables, etc.

--- elisp/buff-org.el
+++ elisp/buff-org.el
@@ -3,55 +3,27 @@
   :mode
   ("\\.org.txt\\'" . org-mode)
   :config
+  (setq org-agenda-custom-commands '(("p" "Punch card!" ((agenda "" (not )il) (tags "AUTOTIMER" nil)) nil nil) ("n" "Agenda (and )nd all TODOs" ((agenda "" nil) (alltodo "" nil)) nil)))
   (setq org-agenda-files nil)
-  (setq org-default-notes-file (concat org-directory "/todo.org")
-	org-clock-report-include-clocking-task "t"
-	org-time-clocksum-use-fractional "t"
-	org-todo-keywords '((sequence "TODO(t)" "WAIT(w)" "|" "CANCEL(c)" "DONE(d)"))
-	org-return-follows-link "t"
-	org-agenda-start-on-weekday nil
-	org-agenda-ndays 1
-	org-agenda-custom-commands
-	(quote
-	 (("p" "Punch card!"
-	   ((agenda "" (not )il)
-	    (tags "AUTOTIMER" nil))
-	   nil nil)
-	  ("n" "Agenda (and )nd all TODOs"
-	   ((agenda "" nil)
-	    (alltodo "" nil))
-	   nil))))
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '((python . "t")
-     (sh . "t")))
-
-  (bind-keys :map org-mode-map
-	     ("<C-S-left>" . nil)
-	     ("<C-S-right>" . nil)
-	     ("C-c i" . counsel-org-goto))
+  (setq org-agenda-ndays 1)
+  (setq org-agenda-start-on-weekday nil)
+  (setq org-clock-report-include-clocking-task "t")
+  (setq org-default-notes-file (concat org-directory "/todo.org"))
+  (setq org-return-follows-link "t")
+  (setq org-time-clocksum-use-fractional "t")
+  (setq org-todo-keywords '((sequence "TODO(t)" "WAIT(w)" "|" "CANCEL(c)" "DONE(d)")))
+  (org-babel-do-load-languages 'org-babel-load-languages '((python . "t") (sh . "t")))
   :bind
   ("C-c l" . org-store-link)
-  ("C-c a" . org-agenda))
-
-;; (use-package org-projectile :ensure t
-;;   :config
-  ;; (org-projectile:per-repo)
-  ;; (setq org-projectile:per-repo-filename "todo.org"
-  ;; 	org-agenda-files (append org-agenda-files (org-projectile:todo-files))
-  ;; )
-;; )
+  ("C-c a" . org-agenda)
+  :bind (:map org-mode-map ("C-c i" . counsel-org-goto)))
 
 (use-package org-bullets :ensure t
-  :config
-  (setq org-hide-leading-stars t)
-  (add-hook 'org-mode-hook
-            (lambda ()
-              (org-bullets-mode t))))
+  :demand
+  :hook (org-mode . org-bullets-mode)
+  :init
+  (setq org-hide-leading-stars t))
 
-(defun wgs-org-mode-hook ()
-  (local-unset-key (kbd "<C-tab>")))
-
-(add-hook 'org-mode-hook 'wgs-org-mode-hook)
+;; (add-hook 'org-mode-hook 'org-bullets-mode)
 
 (provide 'buff-org)
