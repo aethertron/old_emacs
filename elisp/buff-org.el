87d397a0f2a7dd7ae3c1c4c679999e8119d38476
bgs-blue <william.stubler@gmail.com>
2018-02-05 15:25:46 -0700

    org config

--- elisp/buff-org.el
+++ elisp/buff-org.el
@@ -3,17 +3,19 @@
   :mode
   ("\\.org.txt\\'" . org-mode)
   :config
+  (setq org-M-RET-may-split-line nil)
   (setq org-agenda-custom-commands '(("p" "Punch card!" ((agenda "" (not )il) (tags "AUTOTIMER" nil)) nil nil) ("n" "Agenda (and )nd all TODOs" ((agenda "" nil) (alltodo "" nil)) nil)))
   (setq org-agenda-files nil)
   (setq org-agenda-ndays 1)
   (setq org-agenda-start-on-weekday nil)
-  (setq org-clock-report-include-clocking-task "t")
+  (setq org-clock-report-include-clocking-task t)
   (setq org-default-notes-file (concat org-directory "/todo.org"))
-  (setq org-return-follows-link "t")
+  (setq org-insert-heading-respect-content t)
+  (setq org-return-follows-link t)
   (setq org-startup-folded 'nofold)
-  (setq org-time-clocksum-use-fractional "t")
+  (setq org-time-clocksum-use-fractional t)
   (setq org-todo-keywords '((sequence "TODO(t)" "WAIT(w)" "|" "CANCEL(c)" "DONE(d)")))
-  (org-babel-do-load-languages 'org-babel-load-languages '((python . "t") (sh . "t")))
+  (org-babel-do-load-languages 'org-babel-load-languages '((python . t) (sh . t)))
   :bind
   ("C-c l" . org-store-link)
   ("C-c a" . org-agenda)
