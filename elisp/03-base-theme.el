24638f15cb024223a3cf85e5605a45e873426a3c
bgs-blue <william.stubler@gmail.com>
2018-02-06 12:19:47 -0700

    no more theme init by default, reserved for local

--- elisp/03-base-theme.el
+++ elisp/03-base-theme.el
@@ -1,12 +1,12 @@
 (set-face-attribute 'default nil :height 120)
 
-(use-package solarized-theme :ensure t
-  :demand
-  :config
-  (load-theme 'solarized-dark t))
-
-(use-package smart-mode-line-powerline-theme :ensure t)
-(setq sml/theme 'respectful)
-(sml/setup)
+;; (use-package solarized-theme :ensure t
+;;   :demand
+;;   :config
+;;   (load-theme 'solarized-dark t))
+;;
+;; (use-package smart-mode-line-powerline-theme :ensure t)
+;; (setq sml/theme 'respectful)
+;; (sml/setup)
 
 (provide '03-base-theme)
