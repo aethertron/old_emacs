2068099d7ff4ceae1de91f5acfd01474347d78f8
bgs-blue <william.stubler@gmail.com>
2018-03-13 16:20:25 -0700

    hydra window adjustment

--- elisp/05-base-extensions.el
+++ elisp/05-base-extensions.el
@@ -257,6 +257,50 @@ INITIAL-DIRECTORY, if non-nil, is used as the root directory for search."
   (hlinum-activate))
 
 
+(use-package hydra :ensure t :defer t)
+
+(defun hydra-move-splitter-left (arg)
+  "Move window splitter left."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+        (windmove-find-other-window 'right))
+      (shrink-window-horizontally arg)
+    (enlarge-window-horizontally arg)))
+
+(defun hydra-move-splitter-right (arg)
+  "Move window splitter right."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+        (windmove-find-other-window 'right))
+      (enlarge-window-horizontally arg)
+    (shrink-window-horizontally arg)))
+
+(defun hydra-move-splitter-up (arg)
+  "Move window splitter up."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+        (windmove-find-other-window 'up))
+      (enlarge-window arg)
+    (shrink-window arg)))
+
+(defun hydra-move-splitter-down (arg)
+  "Move window splitter down."
+  (interactive "p")
+  (if (let ((windmove-wrap-around))
+        (windmove-find-other-window 'up))
+      (shrink-window arg)
+    (enlarge-window arg)))
+
+(defhydra hydra-splitter (global-map "C-c e")
+    "splitter"
+    ("h" hydra-move-splitter-left)
+    ("j" hydra-move-splitter-down)
+    ("k" hydra-move-splitter-up)
+    ("l" hydra-move-splitter-right))
+
+(use-package hydra :ensure t)
+
+
 (use-package ivy :ensure t
   :init
   (ivy-mode 1)
