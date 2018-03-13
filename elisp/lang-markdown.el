a9b247b793cddd4f556465acbf44be08f98ba1ca
bgs-blue <william.stubler@gmail.com>
2018-03-13 16:20:15 -0700

    magit style headings for markdown

--- elisp/lang-markdown.el
+++ elisp/lang-markdown.el
@@ -1,8 +1,18 @@
+(defun markdown-show-level-1 () (interactive) (markdown-hide-sublevels 1))
+(defun markdown-show-level-2 () (interactive) (markdown-hide-sublevels 2))
+(defun markdown-show-level-3 () (interactive) (markdown-hide-sublevels 3))
+
 (use-package markdown-mode :ensure t
   :mode "\\.txt\\'"
-  :init
+  :custom
   ;; Note: must install markdown-it using npm, supports tables unlike markdown prog
-  (setq markdown-command "markdown-it"))
+  (markdown-command "markdown-it")
+  :config
+  (bind-keys :map markdown-mode-map
+             ("M-1" . markdown-show-level-1)
+             ("M-2" . markdown-show-level-2)
+             ("M-3" . markdown-show-level-3)
+             ("M-4" . markdown-show-all)))
 
 (provide 'lang-markdown)
 ;;; lang-markdown.el ends here
