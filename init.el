817a03cded59e27201f7b116e98c397378d377d6
bgs-blue <william.stubler@gmail.com>
2018-07-09 13:03:11 -0700

    using this new env variable and assigning to a constant

--- init.el
+++ init.el
@@ -12,13 +12,20 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
+;; determine local module path: used for costum and for loading after init
+
+(defconst local-module-path
+  (let ((raw-path (if (not (getenv "EMACS_LOCAL_MODULE_PATH"))
+                    (concat user-emacs-directory "local" "/")
+                    (getenv "EMACS_LOCAL_MODULE_PATH"))))
+    (if (string= (substring raw-path -1) "/") raw-path (concat raw-path "/")))
+  "Path of the local variable used for custom and post-load stuff should end with '/'")
+
 (add-to-list 'load-path (concat user-emacs-directory "elisp"))
 (require '00-init)
 
-;;  load the local moudle, place for version-controlled site-specific stuff
-(let* ((local-feature "local")
-       (local-path (concat user-emacs-directory local-feature "/"))
-       (local-filename (concat local-path local-feature ".el")))
+;;  load the local module, place for version-controlled site-specific stuff
+(let* ((local-filename (expand-file-name "local.el" local-module-path)))
   (when (file-exists-p local-filename)
-    (add-to-list 'load-path local-path)
+    (add-to-list 'load-path local-module-path)
     (require 'local)))
