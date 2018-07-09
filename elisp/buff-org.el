4b76ccec502106daaa4584e2ec5207072ab71964
bgs-blue <william.stubler@gmail.com>
2018-07-09 14:12:59 -0700

    whitespace fixes

--- elisp/buff-org.el
+++ elisp/buff-org.el
@@ -6,46 +6,48 @@
 (defun org-global-show-all () (interactive) (org-global-cycle '(64)))
 
 (use-package org :ensure t
-  :init
-  (org-babel-do-load-languages 'org-babel-load-languages '((python . t) (shell . t)))
-  (setq org-agenda-custom-commands '(("p" "Punch card!" ((agenda "" nil) (tags "AUTOTIMER" nil)) nil nil) ("n" "Agenda (and )nd all TODOs" ((agenda "" nil) (alltodo "" nil)) nil)))
-  :custom
-  (org-M-RET-may-split-line nil)
-  (org-agenda-file-regexp "\\`[^.].*\\.org\\'")
-  (org-agenda-ndays 1)
-  (org-agenda-start-on-weekday nil)
-  (org-agenda-use-time-grid nil "no time grid by default")
-  (org-clock-in-resume t)
-  (org-clock-persist t)
-  (org-clock-persist-file (concat temp-dir "/org-clock-save.el"))
-  (org-clock-report-include-clocking-task t)
-  (org-clock-rounding-minutes t)
-  (org-cycle-open-archived-trees t)
-  (org-default-notes-file (concat "~/todo.org"))
-  (org-insert-heading-respect-content t)
-  (org-return-follows-link t)
-  (org-startup-folded 'nofold)
-  (org-time-clocksum-use-fractional t)
-  (org-time-clocksum-use-fractional t)
-  (org-time-stamp-rounding-minutes '(6 6))
-  (org-todo-keywords '((sequence "TODO(t)" "WAIT(w)" "|" "CANCEL(c)" "DONE(d)")))
-  :mode
-  ("\\.org.txt\\'" . org-mode)
-  :bind
-  ("C-c l" . org-store-link)
-  ("C-c a" . org-agenda)
-  :config
-  (bind-keys :map org-mode-map
-             ("C-c i" . counsel-org-goto)
-             ("C-,"   . nil)
-             ("C-'"   . nil)
-             ("M-1" . org-global-cycle-1)
-             ("M-2" . org-global-cycle-2)
-             ("M-3" . org-global-cycle-3)
-             ("M-4" . org-global-show-all))
-  (bind-keys :map org-agenda-mode-map
-             ("M-n" . org-agenda-next-item)
-             ("M-p" . org-agenda-previous-item)))
+             :init
+             (org-babel-do-load-languages 'org-babel-load-languages '((python . t) (shell . t)))
+             (setq org-agenda-custom-commands '(("p" "Punch card!" ((agenda "" nil) (tags "AUTOTIMER" nil)) nil nil) ("n" "Agenda (and )nd all TODOs" ((agenda "" nil) (alltodo "" nil)) nil)))
+             :custom
+             (org-M-RET-may-split-line nil)
+             (org-agenda-file-regexp "\\`[^.].*\\.org\\'")
+             (org-agenda-ndays 1)
+             (org-agenda-start-on-weekday nil)
+             (org-agenda-use-time-grid nil "no time grid by default")
+             (org-clock-in-resume t)
+             (org-clock-persist t)
+             (org-clock-persist-file (concat temp-dir "/org-clock-save.el"))
+             (org-clock-report-include-clocking-task t)
+             (org-clock-rounding-minutes t)
+             (org-cycle-open-archived-trees t)
+             (org-default-notes-file (concat "~/todo.org"))
+             (org-insert-heading-respect-content t)
+             (org-return-follows-link t)
+             (org-startup-folded 'nofold)
+             (org-time-clocksum-use-fractional t)
+             (org-time-clocksum-use-fractional t)
+             (org-time-stamp-rounding-minutes '(6 6))
+             (org-todo-keywords '((sequence "TODO(t)" "WAIT(w)" "|" "CANCEL(c)" "DONE(d)")))
+             :mode
+             ("\\.org.txt\\'" . org-mode)
+             :bind
+             ("C-c l" . org-store-link)
+             ("C-c a" . org-agenda)
+             :config
+             (bind-keys :map org-mode-map
+                        ("C-c i" . counsel-org-goto)
+                        ("C-,"   . nil)
+                        ("C-'"   . nil)
+                        ("M-1" . org-global-cycle-1)
+                        ("M-2" . org-global-cycle-2)
+                        ("M-3" . org-global-cycle-3)
+                        ("M-4" . org-global-show-all))
+             (bind-keys :map org-agenda-mode-map
+                        ("M-n" . org-agenda-next-item)
+                        ("M-p" . org-agenda-previous-item))
+             (add-hook 'org-agenda-mode-hook (lambda () (require 'org-clock) (org-clock-load)))
+             (add-hook 'kill-emacs-hook (lambda () (require 'org-clock) (org-clock-save))))
 
 (use-package org-bullets :ensure t
   :demand
